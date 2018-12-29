a7045519e1cd7bbf1f2b40e1a44e1d6147b28bd2
段雨田 <prd-yutian.duan@winwayworld.com>
2018-12-29 14:06:17 +0800

    modify

---
+++ YTTool/YTTool/YTToolHeader.h
@@ -0,0 +1,13 @@
+//
+//  YTToolHeader.h
+//  YTTool
+//
+//  Created by Yutian Duan on 2018/12/29.
+//  Copyright © 2018年 Wanwin. All rights reserved.
+//
+
+#ifndef YTToolHeader_h
+#define YTToolHeader_h
+
+
+#endif /* YTToolHeader_h */
---
+++ YTTool/YTTool/you/UIDevice+YTInfo.m
@@ -0,0 +1,13 @@
+//
+//  UIDevice+YTInfo.m
+//  YTTool
+//
+//  Created by Yutian Duan on 2018/12/29.
+//  Copyright © 2018年 Wanwin. All rights reserved.
+//
+
+#import "UIDevice+YTInfo.h"
+
+@implementation UIDevice (YTInfo)
+
+@end
